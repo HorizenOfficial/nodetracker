da788ab86f03bc10f5f235ee7c360cea58570cb3
Allan Dumaine <allan.dumaine@gmail.com>
2018-12-12 12:40:01 -0700

    Add ip6 lookup family (credit to emminer - thanks!)

--- ipv6-dns-workaround.js
+++ ipv6-dns-workaround.js
@@ -2,6 +2,6 @@ const dns = require('dns');
 
 const { lookup } = dns;
 dns.lookup = (name, opts, cb) => {
-  if (typeof cb !== 'function') return lookup(name, { verbatim: true }, opts);
-  return lookup(name, Object.assign({ verbatim: true }, opts), cb);
+  if (typeof cb !== 'function') return lookup(name, { verbatim: true, family: 6 }, opts);
+  return lookup(name, Object.assign({ verbatim: true, family: 6 }, opts), cb);
 };
