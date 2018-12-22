b5f6cc70fbcf7851a4d2b41f1ec5fff9062c5679
Allan Dumaine <allan.dumaine@gmail.com>
2018-12-21 23:01:03 -0700

    Fix dns options to properly combine objects

--- ipv6-dns-workaround.js
+++ ipv6-dns-workaround.js
@@ -3,5 +3,5 @@ const dns = require('dns');
 const { lookup } = dns;
 dns.lookup = (name, opts, cb) => {
   if (typeof cb !== 'function') return lookup(name, { verbatim: true, family: 6 }, opts);
-  return lookup(name, Object.assign({ verbatim: true, family: 6 }, opts), cb);
+  return lookup(name, Object.assign(opts, { verbatim: true, family: 6 }), cb);
 };
